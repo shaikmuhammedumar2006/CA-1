82b691449d1427c63fca3a0eb17e9008ae6f35fc
c.Mokshagna sai <c.sai@s.amity.edu>
2025-03-26 13:42:51 +0530

    case 1

---
+++ L29-30/test.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int choice;
+
+    printf("Select an engineering course by entering the corresponding number:\n");
+    printf("1. Computer Engineering\n");
+    printf("2. Electrical Engineering\n");
+    printf("3. Mechanical Engineering\n");
+    printf("4. Civil Engineering\n");
+    printf("5. Chemical Engineering\n");
+    printf("6. Aerospace Engineering\n");
+    printf("7. Software Engineering\n");
+    printf("8. Environmental Engineering\n");
+    printf("Enter your choice (1-8): ");
+    scanf("%d", &choice);
+
+    switch (choice) {
+        case 1:
+            printf("You selected: Computer Engineering\n");
+            break;
